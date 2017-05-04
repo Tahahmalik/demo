3809321d3eda21a6296bb87a993c10f8c48c4aca
Taha Malik <taha.malik87@gmail.com>
2017-05-04 15:35:13 -0700

    My basic php queries to delete,insert or update record

---
+++ insert.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO MyGuests (firstname , lastname , email) VALUES 
+
+('Affan', 'Malik', 'Affan.m86@gmail.com'),
+('Hasaan' , 'Malik', 'Hasaan_malik@gmail.com')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+
+$conn->close();
+?>
