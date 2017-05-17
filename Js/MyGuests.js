1a05d8fb55ef061beae4430e4f8896c31ffcfd50
Taha Malik <taha.malik87@gmail.com>
2017-05-17 14:12:52 -0700

    Fully functional CRUD application with refactored front/back

--- Js/MyGuests.js
+++ Js/MyGuests.js
@@ -1,67 +1,68 @@
 function load() {
-    $(".tablerows").remove();
-    $.getJSON( "load.php", function( data ) {
+  $(".tablerows").remove();
+  $.getJSON( "load.php", function( data ) {
 
-  $.each(data, function ( key , value) {
-    $(".table").append("<tr class='tablerows'>"+"<td>"+this.id+"</td>"+"<td>"+this.firstname+"</td>"+"<td>"+this.lastname+"</td>"+"<td>"+this.email+"</td>"+"<td>"+this.reg_date+"</td>"+"</tr>");
-  
-  });
+    $.each(data, function ( key , value) {
+
+      var id = this.id;
+
+      $(".table").append("<tr class='tablerows'><td>"+this.id+"</td><td>"+this.firstname+"</td><td>"+this.lastname+"</td><td>"+this.email+"</td><td>"+this.reg_date+"</td><td><img src='images/delete.png' onclick='del("+this.id+")'></td><td><img src='images/edit.png' onclick='changerecord("+id+")'></td></tr>");
+
+    });
   });
 } 
 
 
+
 function update(){
 
-  var fname = document.getElementById("firstname").value;
-    var lname = document.getElementById("lastname").value;
-    var email = document.getElementById("email").value;
-    var id = document.getElementById("id").value;
-    
-    var post = 'firstname='+fname+'&lastname='+lname+'&email='+email+'&id='+id;
-        console.log(post);
+  var fname = document.getElementById("firstnameupdate").value;
+  var lname = document.getElementById("lastnameupdate").value;
+  var email = document.getElementById("emailupdate").value;
+  var id = document.getElementById("idupdate").value;
 
-	
-	xmlhttp = new XMLHttpRequest();
-	xmlhttp.open("POST", "update.php", true);
-	xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  var post = 'firstname='+fname+'&lastname='+lname+'&email='+email+'&id='+id;
+  console.log(post);
+
+
+  xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("POST", "update.php", true);
+  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   
-	
-	 xmlhttp.onreadystatechange = function() {
 
-            if (this.readyState == 4 && this.status == 200) {
-            $(".tablerows").remove();
-            $.getJSON( "load.php", function( data ) {
-            $.each(data, function ( key , value) {
-            $(".table").append("<tr class='tablerows'>"+"<td>"+this.id+"</td>"+"<td>"+this.firstname+"</td>"+"<td>"+this.lastname+"</td>"+"<td>"+this.email+"</td>"+"<td>"+this.reg_date+"</td>"+"</tr>");
-             console.log(this.firstname);
-             });
-            });   
+  xmlhttp.onreadystatechange = function() {
 
-            }
 
-            if (this.readyState == 4 && this.status == 400) {
-               alert("Please enter all information to be updated!!");
-                
-            }
-        };
-       
-        xmlhttp.send(post);
-        document.getElementById("firstname").value = "";
-        document.getElementById("lastname").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("id").value = "";
+
+    if (this.readyState == 4 && this.status == 200) {
+
+     load();
+
+   }
+
+   if (this.readyState == 4 && this.status == 400) {
+     alert("Please enter all information to be updated!!");
+
+   }
+ };
+
+ xmlhttp.send(post);
+
+ $("#updaterecord").css("visibility","hidden"); 
+ $("#updateclick").hide();
+
 }
 
 
 function create(){
-  
+
   var fname = document.getElementById("firstname").value;
-    var lname = document.getElementById("lastname").value;
-    var email = document.getElementById("email").value;
-   
-    
-    var post = 'firstname='+fname+'&lastname='+lname+'&email='+email;
-        console.log(post);
+  var lname = document.getElementById("lastname").value;
+  var email = document.getElementById("email").value;
+
+
+  var post = 'firstname='+fname+'&lastname='+lname+'&email='+email;
+  console.log(post);
 
   
   xmlhttp = new XMLHttpRequest();
@@ -69,67 +70,93 @@ function create(){
   xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   
   
-   xmlhttp.onreadystatechange = function() {
+  xmlhttp.onreadystatechange = function() {
 
-            if (this.readyState == 4 && this.status == 200) {
-            $(".tablerows").remove();
-            $.getJSON( "load.php", function( data ) {
-            $.each(data, function ( key , value) {
-            $(".table").append("<tr class='tablerows'>"+"<td>"+this.id+"</td>"+"<td>"+this.firstname+"</td>"+"<td>"+this.lastname+"</td>"+"<td>"+this.email+"</td>"+"<td>"+this.reg_date+"</td>"+"</tr>");
-             console.log(this.firstname);
-             });
-            });   
+    if (this.readyState == 4 && this.status == 200) {
 
-                
-            }
+      load();
 
-            if (this.readyState == 4 && this.status == 400) {
-               alert("Please enter all information to be updated!!");
-                
-            }
-        }
-       
-        xmlhttp.send(post);
-        document.getElementById("firstname").value = "";
-        document.getElementById("lastname").value = "";
-        document.getElementById("email").value = "";
+    }
+
+    if (this.readyState == 4 && this.status == 400) {
+     alert("Please enter all information to be updated!!");
+
+   }
+ }
+
+ xmlhttp.send(post);
+ document.getElementById("firstname").value = "";
+ document.getElementById("lastname").value = "";
+ document.getElementById("email").value = "";
+ $("#newrecord").css("visibility","hidden");
+ $("#createnew").css("visibility","hidden");
+
 };
 
-function del() {
+function del(id) {
 
-    var id = document.getElementById("recordnumber").value;
-    
-    var post = 'id='+id;
-        
+
+
+  var post = 'id='+id;
+  console.log(post);    
   
   xmlhttp = new XMLHttpRequest();
   xmlhttp.open("POST", "delete.php", true);
   xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   
   
-   xmlhttp.onreadystatechange = function() {
+  xmlhttp.onreadystatechange = function() {
 
-            if (this.readyState == 4 && this.status == 200) {
-            $(".tablerows").remove();
-            $.getJSON( "load.php", function( data ) {
-            $.each(data, function ( key , value) {
-            $(".table").append("<tr class='tablerows'>"+"<td>"+this.id+"</td>"+"<td>"+this.firstname+"</td>"+"<td>"+this.lastname+"</td>"+"<td>"+this.email+"</td>"+"<td>"+this.reg_date+"</td>"+"</tr>");
-             console.log(this.firstname);
-             });
-            });    
-                
-            }
+    if (this.readyState == 4 && this.status == 200) {
+      load();
 
-            if (this.readyState == 4 && this.status == 400) {
-               alert("Please enter the row to be deleted!!");
-                
-            }
-        };
-       
-        xmlhttp.send(post);
-        document.getElementById("recordnumber").value ="";
-        
+    }
+
+    if (this.readyState == 4 && this.status == 400) {
+     alert("Please enter the row to be deleted!!");
+
+   }
+ };
+
+ xmlhttp.send(post);
+
 }
 
 
 
+
+function changerecord(id){
+  $("#updaterecord").css("visibility","visible");
+  $("#updateclick").show();
+
+
+  $.getJSON( "load.php", function( data ) {
+
+    $.each(data, function ( key , value) {
+
+      if (this.id == id) {
+
+        console.log(this.firstname);
+        console.log(this.lastname);
+        console.log(this.email);
+        console.log(this.id);
+        document.getElementById("firstnameupdate").value = this.firstname;
+        document.getElementById("lastnameupdate").value = this.lastname;
+        document.getElementById("emailupdate").value = this.email;
+        document.getElementById("idupdate").value = this.id;     
+        
+      } 
+
+
+    });
+
+  });
+
+}
+
+function createnew() {
+$("#createnew").css("visibility","visible");
+
+$("#newrecord").css("visibility","visible");
+
+}
--- main.php
+++ main.php
@@ -1,43 +1,51 @@
 <head>
-<title> My Basic Page </title>
-
-<script src="js/MyGuests.js"></script>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
+	<title> My Basic Page </title>
+	<script src="js/MyGuests.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
 </head>
 
 <body>
+	<p id="heading">Current data in the table</p><br>
+	
 
+	<table>
+		<tr>
+			<td>
+				<form action="" method="post" id="newrecord">
+					First Name: <input type="text" name="firstname" id="firstname"><br><br>
+					Last Name: <input type="text" name="lastname" id="lastname"><br><br>
+					E-mail: <input type="text" name="email" id="email" >
+				</form>
+				<input type="submit" class="btn btn-success" value="Create Record" name="createnew"  id="createnew" onclick="create()">
+			</td>
+			<td>
 
+				<table id="table" class="table">
+					<th>ID</th>
+					<th>First name</th>
+					<th>Last name</th>
+					<th>Email</th>
+					<th>Registration date</th>
+				</table> <br>
+				<input type="submit" class="btn btn-success" value="Display all" name="load" onclick="load()">
+				<input type="submit" class="btn btn-success" value="Enter New" name="Enter"  onclick="createnew()">
+			</td>
 
-<div >
-<b> Current data in the table</b>
-<table id="table" class="table">
-<th>id</th>
-<th>First name</th>
-<th>Last name</th>
-<th>Email</th>
-<th>Registration date</th>
+			<td>
+				<form action="" method="post" id="updaterecord">
+					First Name: <input type="text" name="firstname" id="firstnameupdate"><br>
+					Last Name: <input type="text" name="lastname" id="lastnameupdate"><br>
+					E-mail: <input type="text" name="email" id="emailupdate" >
+					<input type="text" name="id" id="idupdate">
+				</form>
+				<input type="submit" class="btn btn-success" value="Update" name="update"  id="updateclick" onclick="update()">
 
-</table>
+			</td>
+		</tr>
+	</table>
+	
 
-</div>
-
-<form action="" method="post">
-First Name: <input type="text" name="firstname" id="firstname">
-Last Name: <input type="text" name="lastname" id="lastname">
-E-mail: <input type="text" name="email" id="email" >
-ID: <input type="text" name="id" id="id">
-<br>
-Delete : <input type="integer" name="recordnumber" id="recordnumber">
- 
-
- 
-</form>
-<input type="submit" class="btn btn-success" value="load" name="load" onclick="load()">
-<input type="submit" class="btn btn-success" value="Update" name="update"  onclick="update()">
-<input type="submit" class="btn btn-success" value="Create" name="create"  onclick="create()">    
-<input type="submit" class="btn btn-success" value="Delete" name="Delete"  onclick="del()"><br>
+	<input type="submit" class="btn btn-success" value="Update" name="update"  id="updateclick" onclick="update()">
 
 </body>
